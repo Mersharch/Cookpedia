670efb7a346f7fc9ddf136af7732298e8e72ac6d
Mersharch <mersharchdemenya@icloud.com>
2023-03-07 15:30:17 +0000

    setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+  <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-400 text-xl font-bold mt-2 text-center">
+          Open up App.js to start working on your app!
+          Cookpedia
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
